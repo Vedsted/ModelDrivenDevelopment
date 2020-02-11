f805e034750c5f6b0e5c74d96f31035871f41961
Vedsted <jonso16@student.sdu.dk>
2020-02-11 13:25:13 +0100

    GUI_Exercise added

---
+++ GUI_Exercise/src/qui_generic_model/QButton.java
@@ -0,0 +1,39 @@
+package qui_generic_model;
+
+import javax.swing.*;
+import java.awt.event.ActionListener;
+import java.util.Map;
+
+/**
+ * GUI metamodel: a simple, named button
+ */
+public class QButton extends QComponent {
+    /**
+     * The properties of this element
+     */
+    private String name, text;
+    /**
+     * Create a new Button element with the given properties
+     * @param name the name of the button
+     * @param text the text of the button
+     */
+    public QButton(String name, String text) {
+        this.name = name; this.text = text;
+    }
+    /**
+     * Create the corresponding Swing element, depending on the kind, store in map etc
+     */
+    @Override protected JComponent create(ActionListener handler, Map<String,JComponent> componentMap) {
+        JButton button = new JButton(text);
+        button.setActionCommand(name);
+        button.addActionListener(handler);
+        if(name!=null) componentMap.put(name, button);
+        return button;
+    }
+    /**
+     * For debugging
+     */
+    @Override public String toString() {
+        return "Button[...]";
+    }
+}
---
+++ GUI_Exercise/src/qui_generic_model/QPanel.java
@@ -0,0 +1,58 @@
+package qui_generic_model;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionListener;
+import java.util.Map;
+
+/**
+ * GUI metamodel: a nested panel organizing a number of nested components
+ */
+public class QPanel extends QComponent {
+	/**
+	 * Supported layouts
+	 */
+    public static enum Layout { FLOW, VERTICAL, HORIZONTAL; }
+    /**
+     * The layout of the panel
+     */
+    private Layout layout;
+    /**
+     * Create a panel model with the given layout
+     * @param layout the layout of the panel
+     */
+    public QPanel(Layout layout) { this.layout = layout; }
+    /**
+     * Create JPanel containing the nested components
+     */
+    public JPanel create(ActionListener handler, Map<String,JComponent> componentMap) {
+        JPanel panel = new JPanel();
+        panel.setLayout(getLayout(panel));
+        for(QComponent c: contents) panel.add(c.create(handler,componentMap));
+        return panel;
+    }
+    /**
+     * Create layout manager
+     * @param panel the panel to assign the given layout manager
+     * @return the new layout manager
+     */
+    private LayoutManager getLayout(JPanel panel) {
+        if(layout== Layout.FLOW)
+            return new FlowLayout();
+        else if(layout== Layout.HORIZONTAL)
+            return new BoxLayout(panel,BoxLayout.X_AXIS);
+        else if(layout== Layout.VERTICAL)
+            return new BoxLayout(panel,BoxLayout.Y_AXIS);
+        else
+            throw new Error("Unknown layout: "+layout);
+    }
+    /**
+     * For debugging
+     */
+    @Override public String toString() {
+        StringBuffer result = new StringBuffer("JPanel[");
+        for(QComponent c: contents) result.append(c.toString()+",");
+        result.append("]");
+        return result.toString();
+    }
+}
